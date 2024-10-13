6701fedd2fb7f1cf18c11f80504ef2ee2929824a
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-10-13 20:55:45 +0100

    add some error handling

--- packages/views.py
+++ packages/views.py
@@ -1,5 +1,6 @@
-from rest_framework import renderers
 from adrf import views
+from django.http import Http404
+from rest_framework import renderers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -11,9 +12,13 @@ class PackageView(views.APIView):
     renderer_classes = [renderers.JSONRenderer]
 
     async def get(self, request: Request, package_name: str, range: str | None = None):
-
         if range is None:
             range = "*"
-        package_info = await npm.get_package_with_transitive_dependencies(package_name, range)
-        serializer = PackageSerializer(package_info)
-        return Response(serializer.data)
+        try:
+            package_info = await npm.get_package_with_transitive_dependencies(
+                package_name, range
+            )
+            serializer = PackageSerializer(package_info)
+            return Response(serializer.data)
+        except Exception as e:
+            raise Http404("Invalid package or version")
