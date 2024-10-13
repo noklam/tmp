975a49775d1b12708ec4c17f36e6c1ef951ee4fe
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-10-13 23:18:45 +0100

    add tests

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,8 +1,43 @@
 from django.test import Client
+import pytest
 
 
-def test_get_package():
-    client = Client()
+@pytest.fixture
+def client():
+    return Client()
+
+
+@pytest.fixture
+def latest_version_minimatch():
+    response = {
+        "name": "minimatch",
+        "version": "10.0.1",
+        "dependencies": [
+            {
+                "name": "brace-expansion",
+                "version": "2.0.1",
+                "dependencies": [
+                    {"name": "balanced-match", "version": "1.0.2", "dependencies": []}
+                ],
+            }
+        ],
+    }
+    return response
+
+
+def test_get_package_redirect_without_trailing_slash(client, latest_version_minimatch):
+    response = client.get("/package/minimatch", follow=True)  # redirect
+    assert response.status_code == 200
+    assert response.json() == latest_version_minimatch
+
+
+def test_get_package_without_version(client, latest_version_minimatch):
+    response = client.get("/package/minimatch/")
+    assert response.status_code == 200
+    assert response.json() == latest_version_minimatch
+
+
+def test_get_package(client):
     response = client.get("/package/minimatch/3.1.2")
     assert response.status_code == 200
     assert response.json() == {
@@ -19,3 +54,17 @@ def test_get_package():
         "name": "minimatch",
         "version": "3.1.2",
     }
+
+
+def test_get_package_with_range(client, latest_version_minimatch):
+    response = client.get("/package/minimatch/>=3.1.2")
+    assert response.status_code == 200
+    assert response.json() == latest_version_minimatch
+
+
+def test_get_package_with_invalid_range(client, latest_version_minimatch):
+    range = ">=999.99.9"
+    package_name = "minimatch"
+    response = client.get(f"/package/{package_name}/{range}")
+    assert response.status_code == 200
+    assert response.json() == f"{package_name} version not found: {range}"
