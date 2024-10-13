65e5ffef876a76a7e291f7aa7a54b9df1137f9e0
Nok Lam Chan <nok.lam.chan@quantumblack.com>
2024-10-13 22:52:53 +0100

    Add missing trailing slash for routes

--- packages/urls.py
+++ packages/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from packages import views
 
 urlpatterns = [
-    path("<str:package_name>", views.PackageView.as_view()),
+    path("<str:package_name>/", views.PackageView.as_view()),
     path("<str:package_name>/<str:range>", views.PackageView.as_view()),
 ]
